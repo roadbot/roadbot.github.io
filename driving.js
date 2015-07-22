92cd9c78a116a2a92f4fb93877667ad372e596ed
Emily Wong <emilywong99@gmail.com>
2015-07-21 23:15:44 -0700

    Clock doesn't auto start

--- driving.js
+++ driving.js
@@ -7,17 +7,30 @@ var timer = function(){
 	
 	var text = $('#s').text();
 
+
+	// type="text/javascript"
+	// clock.setTime(00);
+	// clock.stop(function() { 
+		
+	// });
+	// var clock = $('.clock').FlipClock(00);
+
+
 	if(text === "Start Driving!") {
 		$('#s').text('Stop Driving!');
 		$('#Result').text('');
 		//start the timer
 		//call a future function
+		(#startClock);
 	}
 	
-	else if(text === "Stop Driving!") {
+	else if(text === "Stop Driving!" id) {
 		$('#s').text('Start Driving!');
 		$('#Result').text('You drove ' + hours + ' hr ' + min + ' min ' + sec + ' sec! ');
 		//stop the timer, call a stop function
+		// var time = clock.getTime(); 
+		// console.log(time);
+		// clock.setTime(00);
 	}
 }
 
