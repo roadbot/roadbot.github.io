ef2dea1facbd36aa325c4239771610d2816189b0
yaraakiki <yakiki98@gmail.com>
2015-07-23 09:49:17 -0700

    Yara's hours per day

--- driving.js
+++ driving.js
@@ -1,11 +1,13 @@
-// var hours = 25 ;
-// var min = 6;
-// var sec = 34;
+// var hours = 0 ;
+// var minutes = 0;
+// var seconds = 0;
 
 
-var totalHours =0;
+var totalHours =20;
 var totalMinutes = 0;
 var totalSeconds = 0;
+
+
 
 var timer = function(){
 	
@@ -33,10 +35,12 @@ var timer = function(){
 		var minutes = Math.floor(getTime / 60) - (hours*60);
 		var seconds = (getTime - minutes * 60) - (hours*3600) - 1;
 		
+ 
+
 		var totalHours = totalHours + hours; 
 		var totalMinutes = totalMinutes + minutes;
 		var totalSeconds = totalSeconds + seconds;
-		
+
 
 
 
@@ -64,3 +68,36 @@ clock.start(function() {
 		// this (optional) callback will fire each time the clock flips
 	});
 
+
+var firstDay = 0;
+var firstMonth = 0;
+
+
+var dateEntry = function(){
+	firstDay = Number(document.getElementById("firstDay").value);
+	firstMonth = Number(document.getElementById("firstMonth").value);
+	
+
+}
+
+
+
+var averageHours = function(){
+	var d = new Date();
+	var today = d.getDate();
+	var month = d.getMonth()+1;
+	
+
+	var timeLeftInSec = (totalHours * 3600) + (totalMinutes * 60) + totalSeconds;
+
+	var daysLeft = 30 * (firstMonth + 5 - month) + (30 - today) + (30-firstDay) ;
+	
+	var averagePerDay = timeLeftInSec / daysLeft;
+
+	var avghours = Math.floor(averagePerDay / 3600);
+	var avgminutes = Math.round(Math.floor(averagePerDay / 60) - (avghours*60));
+	$('#averageHours').text(avghours + " hr " + avgminutes + " min " );
+
+}
+
+averageHours();
